8cca02a991541a0eccdaa9ae7ddb17781cbe0e2c
Dadir93 <abdifatah.farah@student.moringaschool.com>
2024-01-13 02:59:38 +0300

    commit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 14;
+let num4 = 10;
+let mod = (num3 % num4);
+
+let setOfNumbers = [2, 5, 20, 13];
+let max = Math.max(...setOfNumbers);
